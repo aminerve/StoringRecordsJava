8c0c39db264d17900216125e26e45dd31344a001
aminerve <122647915+aminerve@users.noreply.github.com>
2023-09-06 17:36:29 -0400

    fully functional

--- src/main/java/StoringRecords.java
+++ src/main/java/StoringRecords.java
@@ -8,7 +8,7 @@ public class StoringRecords {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Filename:");
+        System.out.println("Name of the file:");
         String file = scan.nextLine();
 
         ArrayList<Person> records = readRecordsFromFile(file);
@@ -23,7 +23,20 @@ public class StoringRecords {
     public static ArrayList<Person> readRecordsFromFile(String file) {
         ArrayList<Person> persons = new ArrayList<>();
 
-        // Write here the code for reading from file
+        // Write here the code for reading from catalog
+        try ( Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()){
+                String line = reader.nextLine();
+                
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                
+                persons.add(new Person(name, age));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
         // and printing the read records
         return persons;
 
